243d23f46534af5de08d0db89aa2103b9c0066ae
felix <ifyifeanyi301@gmail.com>
2026-01-17 14:24:27 +0100

    made configurations to allow me use bootstrap instead of tailwind

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express"
+import path from "path"
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+});
